bc3a85bd30f04297362df9fae3944204bf6ac4c9
Tien Dang <tien140801@gmail.com>
2023-06-28 15:45:30 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -10,10 +10,6 @@ const app: Express = express();
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
-});
-
 const port = process.env.PORT || 8000
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
